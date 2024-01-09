4e753dcb01efffc415b273c64c1402258148be09
AraujomB <155956028+AraujomB@users.noreply.github.com>
2024-01-09 12:45:25 +0100

    Add files via upload

--- ProyectoPabloAraujo/Inicio.php
+++ ProyectoPabloAraujo/Inicio.php
@@ -28,19 +28,19 @@ if(!isset($_SESSION['iniciado'])){
         print "<tr><td>Usuario:</td><td><input type='text' name='user'></td>
         <td>Contraseña:</td><td><input type='text' name='psswrd'></td>
         <td><input type='submit' name='enviar' value='Enviar'></td>
-        </tr></table><br>¿No tienes cuenta?</td><td><a href='RegistrarUsuario.php'><input type='submit' name='crear' value='Registrarse'></a>
-        </form><iframe id='video' width='560' height='315' src='https://www.youtube.com/embed/zlOnfjtTBic?si=YkVBunnwR96TOrWh' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowfullscreen></iframe>
+        </tr></table></form><br>¿No tienes cuenta?<a href='registrarUsuario.php'><input type='submit' name='crear' value='Registrarse'>
+        <iframe id='video' width='560' height='315' src='https://www.youtube.com/embed/zlOnfjtTBic?si=YkVBunnwR96TOrWh' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowfullscreen></iframe>
             <br><a href='listaPersonajes.php'><input id='botonLista' type='submit' name='lista' value='Ver lista de personajes'></a></center>";
         if(isset($_POST['enviar'])){
             $conexion = mysqli_connect('localhost','root','')
             or die('No se ha podido realizar la conexión');
             mysqli_select_db($conexion,'proyecto_pablo_araujo')
             or die('No se ha podido conectar a la base de datos');
-            $instruccion = "select * from usuarios where login='".$_POST['user']."'";
+            $instruccion = "select login from usuarios where login='".$_POST['user']."'";
             $consulta = mysqli_query($conexion,$instruccion);
             $nfilas = mysqli_num_rows($consulta);
             if($nfilas == 1){
-                $instruccion = "select * from usuarios where login = '".$_POST['user']."' and psswrd = '".$_POST['psswrd']."'";
+                $instruccion = "select login, psswrd from usuarios where login = '".$_POST['user']."' and psswrd = '".$_POST['psswrd']."'";
                 $consulta = mysqli_query($conexion,$instruccion);
                 $nfilas = mysqli_num_rows($consulta);
                 if($nfilas == 1){
